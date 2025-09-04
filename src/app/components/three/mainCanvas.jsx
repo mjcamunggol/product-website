f4a4badcc5c8b411c36071804eac463d09e93c59
rein <mjcamunggol@gmail.com>
2025-09-04 12:25:39 +0800

    3D cube and can rotate it

---
+++ src/app/components/three/mainCanvas.jsx
@@ -0,0 +1,20 @@
+'use client';
+import { Environment, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { MainScene } from "./mainScene";
+
+export function MainCanvas(){
+    return (
+        <div className="w-screen h-screen fixed top-0 left-0">
+            <Canvas
+            shadows
+            dpr={[1,2]}
+            camera={{fov:55, position:[0,0,6]}}
+            >
+                <Environment files="sky.jpg" />
+                <MainScene />
+                <OrbitControls />
+            </Canvas>
+        </div>
+    )
+}
---
+++ src/app/components/three/mainScene.jsx
@@ -0,0 +1,11 @@
+
+export function MainScene(){
+    return (
+        <>
+        <mesh>
+            <boxGeometry args={[1,1,1]} />
+            <meshStandardMaterial color="pink" />
+        </mesh>
+        </>
+    )
+}
